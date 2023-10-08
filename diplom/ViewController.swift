c182998fad017162212af73943eb7a50b42de131
ri.gayfullin <ri.gayfullin@tensor.ru>
2023-10-08 14:32:22 +0300

    Initial Commit

---
+++ diplom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diplom
+//
+//  Created by Руслан Гайфуллин on 08.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
